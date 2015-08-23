35d644da86c26688f1f0fe574ce4fe5a7f9360c1
Waler Garcia <wall.e.006@gmail.com>
2015-08-23 15:36:19 -0400

    home page draft

---
+++ js/cheesus.js
@@ -0,0 +1,24 @@
+$(document).ready( function(){
+	//alert(":)!")
+  $(window).ready( function(){
+
+   $("#carDiv").animate({left: '870px'},2000 );
+    });
+
+  var cheese = true;
+
+    function slideMenuDown(){
+    	if(cheese){
+    		$( "#menuDiv" ).slideDown( 2000 )
+    		cheese = false;
+    	}else{
+    		$( "#menuDiv" ).slideUp( 1000 )
+    		cheese = true;
+    	}
+    };
+    $( "#clickMenu" ).click( function(){
+    	slideMenuDown();
+    })
+
+
+})
